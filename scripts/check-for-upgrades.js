c4d8e756d17c7f7cea476ccf8943f75ef50dc037
team-tf-cdk <84392119+team-tf-cdk@users.noreply.github.com>
2022-11-03 01:09:53 +0100

    chore(deps): upgrade dependencies (#40)
    Upgrades project dependencies. See details in [workflow run].
    
    [Workflow Run]: https://github.com/cdktf/cdktf-provider-vsphere/actions/runs/3382019460
    
    ------
    
    *Automatically created by projen via the "upgrade-main" workflow*

--- scripts/check-for-upgrades.js
+++ scripts/check-for-upgrades.js
@@ -11,7 +11,7 @@ const actions = require("@actions/core");
 
 // set by the projen file that generates this script
 const CONSTRAINT = "~> 2.2";
-const FQ_PROVIDER_NAME = "vsphere";
+const FQ_PROVIDER_NAME = "hashicorp/vsphere";
 
 (async function main() {
   if (await newerMatchingProviderVersionExists()) {
