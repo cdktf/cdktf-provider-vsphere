0289108d2cf703304a4a58d859f3fc1e50a4812e
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2023-10-10 03:07:56 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/compute-cluster-host-group/index.ts
+++ src/compute-cluster-host-group/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/hashicorp/vsphere/2.5.0/docs/resources/compute_cluster_host_group
 // generated from terraform resource schema
 
