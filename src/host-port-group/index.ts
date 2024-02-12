c59f7e7259d35b81325c447d92ef093f7133c593
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-02-12 13:03:00 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/host-port-group/index.ts
+++ src/host-port-group/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/hashicorp/vsphere/2.6.1/docs/resources/host_port_group
 // generated from terraform resource schema
 
