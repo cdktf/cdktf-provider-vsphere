4008873e4614dd0ee35016d33c02a22c9437367f
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-02-26 08:06:18 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@d4ab9b8

--- src/data-vsphere-role/index.ts
+++ src/data-vsphere-role/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/hashicorp/vsphere/2.6.1/docs/data-sources/role
 // generated from terraform resource schema
 
