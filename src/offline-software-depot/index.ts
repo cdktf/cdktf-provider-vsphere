194ef6e5cb1852030cb4dc00f2b5ffd5da7beff9
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-08-05 11:26:46 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@3b3b11b

--- src/offline-software-depot/index.ts
+++ src/offline-software-depot/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/hashicorp/vsphere/2.8.2/docs/resources/offline_software_depot
 // generated from terraform resource schema
 
