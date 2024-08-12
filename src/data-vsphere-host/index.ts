989e2c4788d288bdfe23dea7fc00f302becbde50
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-08-12 13:18:45 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@74a2751

--- src/data-vsphere-host/index.ts
+++ src/data-vsphere-host/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/hashicorp/vsphere/2.8.2/docs/data-sources/host
 // generated from terraform resource schema
 
