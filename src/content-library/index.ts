d0f0241cbd165bed5f766651a1315577e3fc6780
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-08-15 12:29:08 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@a9a522c

--- src/content-library/index.ts
+++ src/content-library/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/hashicorp/vsphere/2.8.3/docs/resources/content_library
 // generated from terraform resource schema
 
