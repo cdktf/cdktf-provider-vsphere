ac74aea65845d2234f4f056a240262e7e6c5da87
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-08-25 06:16:59 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@ffdc145

--- src/drs-vm-override/index.ts
+++ src/drs-vm-override/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/hashicorp/vsphere/2.8.3/docs/resources/drs_vm_override
 // generated from terraform resource schema
 
