44e562efa618fba7bb18b0a15a0004cb549f3ea2
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-09-04 03:39:10 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/data-vsphere-custom-attribute/index.ts
+++ src/data-vsphere-custom-attribute/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/hashicorp/vsphere/2.9.0/docs/data-sources/custom_attribute
 // generated from terraform resource schema
 
