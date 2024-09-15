3f21469c61861ffba68fd487fd363f7365e0097f
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-09-15 06:18:56 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@321d292

--- src/data-vsphere-host-pci-device/index.ts
+++ src/data-vsphere-host-pci-device/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/hashicorp/vsphere/2.9.1/docs/data-sources/host_pci_device
 // generated from terraform resource schema
 
--- src/data-vsphere-tag-category/index.ts
+++ src/data-vsphere-tag-category/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/hashicorp/vsphere/2.9.1/docs/data-sources/tag_category
 // generated from terraform resource schema
 
