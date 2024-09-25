972d51c9735fa0aa986e32bc56e5b887dc68952a
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-09-25 10:34:45 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/entity-permissions/index.ts
+++ src/entity-permissions/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/hashicorp/vsphere/2.9.2/docs/resources/entity_permissions
 // generated from terraform resource schema
 
