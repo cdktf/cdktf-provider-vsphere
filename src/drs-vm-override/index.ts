c33630359d29ed7052f9c9a398d152b5779fd722
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2025-01-06 17:38:26 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/drs-vm-override/index.ts
+++ src/drs-vm-override/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/hashicorp/vsphere/2.10.0/docs/resources/drs_vm_override
 // generated from terraform resource schema
 
