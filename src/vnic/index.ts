bd20fa1445b5b92c94bff50fe2cc81035600d3ab
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2025-01-12 06:16:43 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@179cf7e

--- src/vnic/index.ts
+++ src/vnic/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/hashicorp/vsphere/2.10.0/docs/resources/vnic
 // generated from terraform resource schema
 
